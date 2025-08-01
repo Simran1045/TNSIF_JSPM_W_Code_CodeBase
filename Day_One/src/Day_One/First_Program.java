918a311c3b87dbcf5692af4732f7cbe1aa119b5d
Simran Shaikh <Simran Shaikh@LAPTOP-6P358OVQ>
2025-08-01 13:22:10 +0530

    Day_2 programs

--- Day_One/src/Day_One/First_Program.java
+++ Day_One/src/Day_One/First_Program.java
@@ -1,9 +1,79 @@
 // TNSIF TRaining 1_Day - Installation & project push into Github
+//package Day_One;
+//
+//public class First_Program {
+//
+//	public static void main(String[] args) {
+//	   System.out.println("---Hi I'm Simran, How Are You Doinn??--- ");
+//	}
+//}
+
+
+// Day_2 : Program For Variable declaration
+
+//package Day_One;
+//
+//public class First_Program{
+//	
+//	public static void main(String[] args) {
+//		// variable initialization
+//		int $Num = 40;
+//		System.out.println("Value Of The Number Variable Is :"+ $Num);
+//		
+//		// Variable Declaration
+//		
+//		String StudentName = "SIMRAN";
+//		System.out.println("Value Of Name is :"+StudentName);
+//	}
+//}
+
+
+// Data Types Program - Primitive
 package Day_One;
 
-public class First_Program {
-
+public class First_Program{	
+	
 	public static void main(String[] args) {
-	   System.out.println("---Hi I'm Simran, How Are You Doinn??--- ");
+		// Data Types Program - Primitive
+		
+		int value_1 = 24/2;
+		float Value_2 = 30f / 2f;     // last always f,l,d for double, float, long data type
+		double Value_3 = 200d / 5d;
+		System.out.println("The Division result is: "+Value_1,Value_2,Value_3);
+		
+		// Character Declaration
+		char ch = 'S'; 
+		System.out.println("Character is:"+ch);
+		
+		// Assigning num to char
+		char ch1 = 65;    // ASCII syntax
+		System.out.println(ch1);
+		
+		// Area Of Circle
+		
+		int r=4;
+		float pi = 3.14f;
+		double area = 3.14*r*r;
+		System.out.println("The Area Of Circle is :" +area);
+		
+		// byte takes a byte
+		
+		byte byteMax = 127;
+		byte byteMin = -128;
+		System.out.println("Min Range Of byte is:" +byteMax);
+		System.out.println("Min Range Of byte is:" +byteMin);
+		
+		// Short 2 byte
+		
+		short shortMax = 32767;
+	    short shortMax = -32768;	
+	    
+	    // int 4 byte 
+	    long Mobile_Num=9881797923L;
+	    System.out.println("My mobile number is :" +Mobile_Num);
+	    
+				
 	}
 }
+
+
