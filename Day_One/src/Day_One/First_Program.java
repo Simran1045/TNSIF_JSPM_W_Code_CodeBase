24bd4d8559f34ab35b9350f714daa63e7fa9e9e7
Simran Shaikh <Simran Shaikh@LAPTOP-6P358OVQ>
2025-08-01 11:51:29 +0530

    First

---
+++ Day_One/src/Day_One/First_Program.java
@@ -0,0 +1,9 @@
+// TNSIF TRaining 1_Day - Installation & project push into Github
+package Day_One;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+	   System.out.println("---Hi I'm Simran, How Are You Doinn??--- ");
+	}
+}
